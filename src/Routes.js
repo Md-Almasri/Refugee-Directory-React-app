b32fbdf3c2c2912eec162d1ada29f847ca3fbec9
Michael <Michael.Antczak@gmail.com>
2017-08-27 12:11:41 +0100

    Add login form

--- src/Routes.js
+++ src/Routes.js
@@ -4,10 +4,13 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './containers/Home';
 import NotFound from './containers/NotFound';
 import './containers/NotFound.css';
+import Login from './containers/Login';
+import './containers/Login.css';
 
 export default () => (
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route path="/login" exact component={Login} />
     <Route component={NotFound} />
   </Switch>
 );
