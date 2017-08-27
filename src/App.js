c368d1d3072193aa366a937f5b87c79329019c38
Michael <Michael.Antczak@gmail.com>
2017-08-27 11:43:28 +0100

    Add outer chrome
    	- create routes
    	- add Home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar } from 'react-bootstrap';
 import './App.css';
+import Routes from './Routes';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to Refugee Directory</h2>
-        </div>
+      <div className="App container">
+
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">Refugee Directory</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+        </Navbar>
+
+        <Routes />
+
       </div>
     );
   }
---
+++ src/containers/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Refugee Directory</h1>
+          <p>A list of services.</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
